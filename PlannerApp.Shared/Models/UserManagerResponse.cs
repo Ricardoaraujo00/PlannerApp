1722adf62d0a9b62ab3aa298ff1fc83edbb2eea8
Ricardo Araújo <ricardo.araujo.t0114627@edu.atec.pt>
2020-08-24 01:56:11 +0100

    Initial commit on 08 stage

---
+++ PlannerApp.Shared/Models/UserManagerResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlannerApp.Shared.Models
+{
+    public class UserManagerResponse
+    {
+        public string message { get; set; }
+
+        public bool isSuccess { get; set; }
+
+        public string[] Errors { get; set; }
+
+        public string userInfo { get; set; }
+
+        public DateTime? ExpireDate { get; set; }
+
+    }
+}
